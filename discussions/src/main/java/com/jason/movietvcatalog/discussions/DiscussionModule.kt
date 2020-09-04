a9363fc2fa6f8fa2e8e6dd84e98be70204f5ed9d
unknown <jpsunarji21@gmail.com>
2020-09-04 13:08:18 +0800

    perbaikan

--- discussions/src/main/java/com/jason/movietvcatalog/discussions/DiscussionModule.kt
+++
@@ -1,8 +0,0 @@
-package com.jason.movietvcatalog.discussions
-
-import org.koin.android.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val discussionModule = module {
-    viewModel { DiscussionViewModel(get()) }
-}
--- discussions/src/main/java/com/jason/movietvcatalog/discussions/DiscussionViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.jason.movietvcatalog.discussions
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.jason.movietvcatalog.core.domain.usecase.MovieUseCase
-
-class DiscussionViewModel (movieUseCase: MovieUseCase): ViewModel(){
-    val movie = movieUseCase.getAllMovies("").asLiveData()
-}
